792a7138f001d9b2cdd89f7a148494efd6d39778
ThomasBouriaud <t.bouriaud@hotmail.fr>
2018-02-13 11:16:20 +0100

    debut

--- controllers/PiloteController.js
+++ controllers/PiloteController.js
@@ -1,8 +1,15 @@
 
 // ///////////////////////// R E P E R T O I R E    D E S    P I L O T E S
 
-module.exports.Repertoire = 	function(request, response){
+module.exports.ListerPilote = function(request, response){
    response.title = 'Répertoire des pilotes';
-
-      response.render('repertoirePilotes', response);
-  } ;
+    model.getListePilote( function (err, result) {
+        if (err) {
+            // gestion de l'erreur
+            console.log(err);
+            return;
+        }
+        response.listePilote = result;
+        response.render('repertoirePilotes', response);
+});
+}
